a4d7304c822bcc53a4dc8af2c81d1e6537b4f167
Navaa Joshua <navaajoshua0@gmail.com>
2026-04-28 13:54:07 +0530

    fix db connection using railway url

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ import mysql.connector
 from urllib.parse import urlparse
 
 def get_connection():
-    db_url = os.getenv("mysql://root:ziYosEUbDsXUnJxViEMKoZZFkfOIkDJX@switchback.proxy.rlwy.net:31890/railway")
+    db_url = os.getenv("MYSQL_PUBLIC_URL")
 
     if not db_url:
         raise Exception("Database URL not found")
