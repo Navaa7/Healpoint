055736f4b41cccea2dc58a821abb6ef86e22a2ff
Navaa Joshua <navaajoshua0@gmail.com>
2026-04-28 13:45:36 +0530

    fix db connection using railway url

--- db_config.py
+++ db_config.py
@@ -1,9 +1,19 @@
+import os
 import mysql.connector
+from urllib.parse import urlparse
 
 def get_connection():
+    db_url = os.getenv("mysql://root:ziYosEUbDsXUnJxViEMKoZZFkfOIkDJX@switchback.proxy.rlwy.net:31890/railway")
+
+    if not db_url:
+        raise Exception("Database URL not found")
+
+    url = urlparse(db_url)
+
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="11012006",
-        database="hospital_finder"
+        host=url.hostname,
+        user=url.username,
+        password=url.password,
+        database=url.path[1:],  # remove '/'
+        port=url.port
     )
